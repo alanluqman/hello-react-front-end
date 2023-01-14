80e6faf6533b62fd0ff1ae77aa7d69efeb1eb164
alanluqman <35001742+alanluqman@users.noreply.github.com>
2023-01-14 16:40:32 +0300

    Create full app

---
+++ src/components/greeting.js
@@ -0,0 +1,23 @@
+import { useQuery } from '@tanstack/react-query';
+import axios from 'axios';
+import React from 'react'
+import { HashLoader } from 'react-spinners';
+
+export default function Greeting() {
+    const {data, isLoading} = useQuery(
+    ['getGreetings'],
+    async () => {
+      const res = await axios.get('http://127.0.0.1:3000');
+      return res.data;
+    }
+  );
+  if (isLoading){
+    return <HashLoader color='lightblue' />
+  }
+  const random = Math.floor(Math.random() * data.length);
+  return (
+    <div className = "line-1 anim-typewriter">
+      {data[random].greet}
+    </div>
+  )
+}
