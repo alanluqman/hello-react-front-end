042ae037827ccc142d6e738a3b18445413e605e7
alanluqman <35001742+alanluqman@users.noreply.github.com>
2023-01-14 17:05:20 +0300

    Update dependencies

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
 import './App.css';
+import React from 'react';
 import {
   QueryClient,
   QueryClientProvider,
-} from '@tanstack/react-query'
-
+} from '@tanstack/react-query';
 import Greeting from './components/greeting';
 
-
-const queryClient = new QueryClient()
+const queryClient = new QueryClient();
 
 function App() {
   return (
