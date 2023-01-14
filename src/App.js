49a1b9cd2f89319390889055639e2f142e8f7d07
alanluqman <35001742+alanluqman@users.noreply.github.com>
2023-01-14 17:22:50 +0300

    Fix linter error

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
+/* eslint-disable import/no-extraneous-dependencies */
 import './App.css';
 import React from 'react';
 import {
   QueryClient,
   QueryClientProvider,
 } from '@tanstack/react-query';
+import { Route, Routes } from 'react-router-dom';
 import Greeting from './components/greeting';
 
 const queryClient = new QueryClient();
@@ -11,7 +13,9 @@ const queryClient = new QueryClient();
 function App() {
   return (
     <QueryClientProvider client={queryClient}>
-      <Greeting />
+      <Routes>
+        <Route path="/" element={<Greeting />} />
+      </Routes>
     </QueryClientProvider>
   );
 }
